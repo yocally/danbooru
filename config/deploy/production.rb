bb17806be6b60ec1e0b768ba498032433ce1e280
Cally Sibben <csibben@memebase.local>
2017-12-19 14:36:53 -0600

    configured for deployment to memebase.local

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,5 @@
-set :user, "albert"
+set :user, "danbooru"
 set :rails_env, "production"
-server "sonohara.donmai.us", :roles => %w(web app db), :primary => true, :user => "albert"
-server "hijiribe.donmai.us", :roles => %w(web app), :user => "albert"
+server "memebase.local", :roles => %w(web app db), :primary => true, :user => "danbooru"
 
 set :linked_files, fetch(:linked_files, []).push(".env.production")
